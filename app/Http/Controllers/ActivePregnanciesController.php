f5780a663407648628cd15508ec6657040b090a0
Sakis Tosounidis <sakis.tosounidis@gmail.com>
2017-12-22 12:15:54 +0200

    search option for pregnants

--- app/Http/Controllers/ActivePregnanciesController.php
+++ app/Http/Controllers/ActivePregnanciesController.php
@@ -12,8 +12,17 @@ class ActivePregnanciesController extends Controller
         $this->middleware('auth');
     }
 
+
     public function index()
     {
+        if (request('search')) {
+            $pregnancies = Pregnancy::where('finished', false)->with(['patient' => function ($query){
+                $query->where('last_name', 'like', '%' . request('search') . '%')->
+                orWhere('first_name', 'like', '%' . request('search') . '%')->
+                orWhere('middle_name', 'like', '%' . request('search') . '%');
+            }])->paginate(12);
+            return view('patients.active_pregnancies.index', compact('pregnancies'));
+        }
         $pregnancies = Pregnancy::where('finished', false)->paginate(12);
         return view('patients.active_pregnancies.index', compact('pregnancies'));
     }
--- resources/views/patients/active_pregnancies/index.blade.php
+++ resources/views/patients/active_pregnancies/index.blade.php
@@ -11,7 +11,25 @@
                 <div class="card">
 
                     <div class="card-header">
-                        Active Pregnancies
+                        <div class="row">
+                            <div class="col-md-4  my-1">
+                                <div class="form-group">
+                                    <form method="get" action="{{ route('pregnancies') }}" role="search">
+                                        <div class="input-group">
+                                            <input type="text" name="search" class="form-control">
+                                            <span class="input-group-btn">
+                                                <button type="submit" class="btn btn-secondary">
+                                                    <i class="fa fa-search"></i>
+                                                </button>
+                                             </span>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="col-md-6 mt-2">
+                                <p class="lead font-weight-bold text-center">Active Pregnancies</p>
+                            </div>
+                        </div>
                     </div>
 
                     <div class="card-body">
@@ -27,6 +45,7 @@
                             </thead>
                             <tbody>
                             @foreach($pregnancies as $pregnancy)
+                                @if($pregnancy->patient)
                                 <tr>
                                     <td><a href="{{ route ('pregnancies.show', [$pregnancy->patient, $pregnancy]) }}">{{ $pregnancy->patient->last_name }}</a></td>
                                     <td><a href="{{ route ('pregnancies.show', [$pregnancy->patient, $pregnancy]) }}">{{ $pregnancy->patient->first_name }}</a></td>
@@ -34,6 +53,7 @@
                                     <td><a href="{{ route ('pregnancies.show', [$pregnancy->patient, $pregnancy]) }}">{{ $pregnancy->edd ? $pregnancy->edd->format('d.m.Y') : 'no date' }}</a></td>
                                     <td><a href="{{ route ('pregnancies.show', [$pregnancy->patient, $pregnancy]) }}">{{ $pregnancy->corrected_edd ? $pregnancy->corrected_edd->format('d.m.Y') : 'no date' }}</a></td>
                                 </tr>
+                                @endif
                             @endforeach
                             </tbody>
                         </table>
